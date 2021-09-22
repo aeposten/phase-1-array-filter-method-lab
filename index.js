e3689456cf8e104f462044d9423b54a0eb4ca7a5
Amy Posten <amyposten85@gmail.com>
2021-09-22 13:25:56 -0400

    commit tree 856503a227810b37bc91f8af52e0361a5472c1dc

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+let findMatching = (drivers, string) => {
+    let matchingDrivers = drivers.filter(drivers => string.toLowerCase() === drivers.toLowerCase())
+        return matchingDrivers; 
+}
+
+let fuzzyMatch = (drivers, string) => {
+    let matchingLetters = drivers.filter(drivers => string === drivers.slice(0, string.length))
+        return matchingLetters;
+} 
+
+let matchName = (drivers, string) => {
+    let matchingProperty = drivers.filter(drivers => drivers.name === string)
+        return matchingProperty;
+}
